991fb065d9d1e8d1c998de08f933adafa5e6282d
Daniel Caceres <daacaceressa@unal.edu.co>
2017-05-22 20:30:52 -0500

    Improve task_parallelization.cpp

--- input-code/Examples/task_parallelization.cpp
+++ input-code/Examples/task_parallelization.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 typedef vector<vector<int>> graph;
 
+const int MAX_N = int( 1e5 );
+
+double board[ MAX_N ][ MAX_N ];
+int arr[ MAX_N ];
+
+void init() {
+    for( int i = 0; i < MAX_N; ++i )
+        for( int j = 0; j < MAX_N; j++ )
+            board[ i ][ j ] = i*j;
+
+    for( int i = 1; i < MAX_N; ++i )
+        arr[ i ] = arr[ i-1 ] ^ i;
+}
+
 int main( ) {
+    init();
 
     graph A( 10 );
     for( int i = 0, current = 0; i < A.size(); ++i ) {
